db89869f3dd90d2ace1d821793d039f9d711d718
Youssef Ali <youssef.ali@stud.th-deg.de>
2025-06-04 00:55:27 +0200

    Upload New File

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('userMessage', (msg) => {
+    console.log('User:', msg);
+    socket.emit('botMessage', `You said: "${msg}"`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
