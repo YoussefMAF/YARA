0a819da513fb9144756e34583a80fdcfc95f511d
Alex Kerefeyski <alex.kerefeyski@stud.th-deg.de>
2025-07-06 15:33:39 +0200

    Edit server.js

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,9 @@ const io = new Server(server, {
   }
 });
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Conversation flow
